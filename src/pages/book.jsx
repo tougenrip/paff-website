a621c71fe3c9e7ab9b7a655f70025a66903a2000
EgeAkyoldas <akyoldasege@gmail.com>
2023-04-06 13:12:31 +0300

    first commit

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -2,80 +2,80 @@ import React from 'react';
 import Navbar from '../components/navbar';
 
 const CoverBook = () => {
-  return (
-    <>
+    return (
+        <>
 
-        <div className='bg-Book bg-cover bg-[center_top] bg-opacity-40'>
-            <div className='container max-w-[100vw] relative h-[100vh] mx-auto text-white'>
-            <div className='row absolute top-[35vh] right-0 left-8 md:left-24 mx-auto text-start font-Gilroy font-bold text-4xl md:text-6xl lg:text-7xl'>
-                <h1 className='mx-auto font-thin'>THE BOOK</h1>
-                <h2 className='mx-auto'>LEARN CULTURE<br /><span className='font-thin'>AND</span> BE DE<br className='block md:collapse'/>CENTRALIZED</h2>
-            </div>
-        </div>
-        <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
-            <div className="relative   mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
-                <div className="row absolute w-full top-[34vh] align-middle">
-                    <div className="">
-                        <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Welcome WEB 3.0</h1>
-                        <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.
-</h2>
+            <div className='bg-Book bg-cover bg-[center_top] bg-opacity-40'>
+                <div className='container max-w-[100vw] relative h-[100vh] mx-auto text-white'>
+                    <div className='row absolute top-[35vh] right-0 left-8 md:left-24 mx-auto text-start font-Gilroy font-bold text-4xl md:text-6xl lg:text-7xl'>
+                        <h1 className='mx-auto font-thin'>THE BOOK</h1>
+                        <h2 className='mx-auto'>LEARN CULTURE Some<br /><span className='font-thin'>AND</span> BE DE<br className='block md:collapse' />CENTRALIZED</h2>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
-            <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
-                <div className="row absolute w-full top-[34vh] align-middle">
-                    <div className="">
-                        <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Community Basics</h1>
-                        <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.</h2>
+                <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
+                    <div className="relative   mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
+                        <div className="row absolute w-full top-[34vh] align-middle">
+                            <div className="">
+                                <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Welcome WEB 3.0</h1>
+                                <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.
+                                </h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
-            <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
-                <div className="row absolute w-full top-[34vh] align-middle">
-                    <div className="">
-                        <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Trade Basics</h1>
-                        <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.</h2>
+                <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
+                    <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
+                        <div className="row absolute w-full top-[34vh] align-middle">
+                            <div className="">
+                                <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Community Basics</h1>
+                                <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
-            <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
-                <div className="row absolute w-full top-[34vh] align-middle">
-                    <div className="">
-                        <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Ecosystem Basics</h1>
-                        <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.
-</h2>
+                <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
+                    <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
+                        <div className="row absolute w-full top-[34vh] align-middle">
+                            <div className="">
+                                <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Trade Basics</h1>
+                                <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
-            <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
-                <div className="row absolute w-full top-[34vh] align-middle">
-                    <div className="">
-                        <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Follow Future with Us</h1>
-                        <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.</h2>
+                <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
+                    <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
+                        <div className="row absolute w-full top-[34vh] align-middle">
+                            <div className="">
+                                <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Ecosystem Basics</h1>
+                                <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.
+                                </h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
+                    <div className="relative  mt-12 mx-auto text-start text-white h-[90vh] font-Gilroy font-extrabold">
+                        <div className="row absolute w-full top-[34vh] align-middle">
+                            <div className="">
+                                <h1 className='text-start mr-auto text-2xl md:text-5xl w-[85%]'>Follow Future with Us</h1>
+                                <h2 className='text-start font-extralight  mr-auto text-xl md:text-2xl mt-8 w-[85%]'>Paff Studios envisions a future where we stand as an eminent pioneer in the global blockchain sphere, orchestrating a harmonious fusion of innovative, sustainable digital ecosystems that empower individuals, enterprises, and societies to seamlessly interconnect, collaborate, and flourish within a decentralized realm.</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
+                    <a href='/insidebook'>
+                        <div className='absolute text-center bottom-1/2 bg-[#141414] p-16 rounded-full  outline'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3.5} stroke="rgb(255, 255, 255)" className="w-16 h-16 left-0 right-0">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
-        <div className='container max-w-[90vw] relative h-[100vh] mx-auto text-white '>
-            <a href='/insidebook'>
-            <div className='absolute text-center bottom-1/2 bg-[#141414] p-16 rounded-full  outline'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3.5} stroke="rgb(255, 255, 255)" className="w-16 h-16 left-0 right-0">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
-            </div>
-            </a>
-        </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default CoverBook
